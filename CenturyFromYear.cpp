fda3f56980f41d36d7c9c43df940cd699d1d073f
visheshbhat123 <111527919+visheshbhat123@users.noreply.github.com>
2022-11-20 21:48:25 -0800

    Create CenturyFromYear.cpp

---
+++ CenturyFromYear.cpp
@@ -0,0 +1,44 @@
+int solution(int year) {
+if (year >=1 & year <= 100)
+return 1;
+else if (year >=101 & year <= 200) 
+return 2;
+else if (year >=201 & year <= 300) 
+return 3;
+else if (year >=301 & year <= 400) 
+return 4;
+else if (year >=401 & year <= 500) 
+return 5;
+else if (year >=501 & year <= 600) 
+return 6;
+else if (year >=601 & year <= 700) 
+return 7;
+else if (year >=701 & year <= 800) 
+return 8;
+else if (year >=801 & year <= 900) 
+return 9;
+else if (year >=901 & year <= 1000) 
+return 10;
+else if (year >=1001 & year <= 1100) 
+return 11;
+else if (year >=1101 & year <= 1200) 
+return 12;
+else if (year >=1201 & year <= 1300) 
+return 13;
+else if (year >=1301 & year <= 1400) 
+return 14;
+else if (year >=1401 & year <= 1500) 
+return 15;
+else if (year >=1501 & year <= 1600) 
+return 16;
+else if (year >=1601 & year <= 1700) 
+return 17;
+else if (year >=1701 & year <= 1800) 
+return 18;
+else if (year >=1801 & year <= 1900) 
+return 19;
+else if (year >=1901 & year <= 2000) 
+return 20;
+else if (year >=2001 & year <= 2100) 
+return 21;
+}
