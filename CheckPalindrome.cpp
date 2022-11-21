1c3f5bb4a612456a4cd0e12aa5b4ef3a03e6b609
visheshbhat123 <111527919+visheshbhat123@users.noreply.github.com>
2022-11-20 21:53:31 -0800

    Update CheckPalindrome.cpp

--- CheckPalindrome.cpp
+++ CheckPalindrome.cpp
@@ -1,3 +1,14 @@
+/*Given the string, check if it is a palindrome.
+
+Example
+
+For inputString = "aabaa", the output should be
+solution(inputString) = true;
+For inputString = "abac", the output should be
+solution(inputString) = false;
+For inputString = "a", the output should be
+solution(inputString) = true.*/
+
 bool solution(string S) {
   for (int i = 0; i < S.length() / 2; i++) {
  
