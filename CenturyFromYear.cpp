96ba6574a7d060f0ca3c5a9e714fa666ed0d7299
visheshbhat123 <111527919+visheshbhat123@users.noreply.github.com>
2022-11-20 21:52:09 -0800

    Update CenturyFromYear.cpp

--- CenturyFromYear.cpp
+++ CenturyFromYear.cpp
@@ -1,3 +1,12 @@
+/*Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
+
+Example
+
+For year = 1905, the output should be
+solution(year) = 20;
+For year = 1700, the output should be
+solution(year) = 17.*/
+
 int solution(int year) {
 if (year >=1 & year <= 100)
 return 1;
