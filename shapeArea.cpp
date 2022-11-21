268daf673137dce289aba51e6c8b8bd383364e25
visheshbhat123 <111527919+visheshbhat123@users.noreply.github.com>
2022-11-20 21:55:08 -0800

    Create shapeArea.cpp

---
+++ shapeArea.cpp
@@ -0,0 +1,23 @@
+/*Below we will define an n-interesting polygon. Your task is to find the area of a polygon for a given n.
+
+A 1-interesting polygon is just a square with a side of length 1. An n-interesting polygon is obtained by taking the n - 1-interesting polygon and appending 1-interesting polygons to its rim, side by side. You can see the 1-, 2-, 3- and 4-interesting polygons in the picture below.
+
+
+
+Example
+
+For n = 2, the output should be
+solution(n) = 5;
+For n = 3, the output should be
+solution(n) = 13.*/
+
+int solution(int n) {
+    int s;
+    if (n == 0)
+    {s = 0;}
+    if (n == 1)
+    {s = 1;}
+    else
+    s = solution(n-1) + 4*(n-1);
+return s;
+}
